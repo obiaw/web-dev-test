69533a8c6a9f32ff1fdaa05d11e7973b8f8ac59c
=obia williams <=obia.williams@gmail.com>
2018-06-25 13:32:24 +0300

    added getting user input from form function and formatting the response ready for web api

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,45 @@ var router = express.Router();
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Awamon Web Developer Exam' });
 });
 
-// perform math operations
 
-router.get('/operations', function(req,res){
-  
+// post form when user fills it in
+router.post('/operations', function(req,res, next){
+  //get user input from form
+  var opd1 = parseInt(req.body.number1);
+  var opd2 = parseInt(req.body.number2);
+  var opn = req.body.operation;
+  // var ans;
+
+  //   switch(opn){
+  //     case "ADD":
+  //         ans = opd1 + opd2;
+  //         return ans;
+  //         break;
+  //     case "SUB":
+  //         ans = opd1 - opd2;
+  //         return ans;
+  //         break;
+  //     case "MUL":
+  //         ans = opd1 * opd2;
+  //         return ans;
+  //         break;
+  //     case "DIV":
+  //         ans = opd1 / opd2;
+  //         return ans;
+  //         break;
+
+  //     default:
+  //     break
+
+  //   };
+
+  var response = {opd1:opd1,opd2:opd2, opn:opn};
+
+  console.log(response);
+  res.send(response);
 
 });
 
