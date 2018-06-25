05a65ec3b69a12f515f639f9054c59ea37dad0f2
=obia williams <=obia.williams@gmail.com>
2018-06-25 11:58:16 +0300

    added from UI and table for displaying results

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+// perform math operations
+
+router.get('/operations', function(req,res){
+  
+
 });
 
 module.exports = router;
